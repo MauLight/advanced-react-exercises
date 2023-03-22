0c015f0bd099303c7196881f0b622318b5c95c81
MauLight <maulisseluz@gmail.com>
2023-03-22 13:47:17 -0300

    first exercise done

---
+++ src/objects.js
@@ -0,0 +1,23 @@
+export const topDesserts = [
+    {
+        id: "1",
+        title: "Tiramisú",
+        description: "The best in town",
+        image: "https://www.elmundoeats.com/wp-content/uploads/2020/12/FP-10-Minute-Eggless-Tiramisu-For-Two-2.jpg",
+        price: "$5.00"
+    },
+    {
+        id: "2",
+        title: "Lemon Ice Cream",
+        description: "Mind-blowing taste",
+        image: "https://www.yourhomebasedmom.com/wp-content/uploads/2013/07/lemon-ice-cream-5-1-500x500.jpg",
+        price: "$4.50"
+    },
+    {
+        id: "3",
+        title: "Chocolate mousse",
+        description: "Unexplored flavour",
+        image: "https://www.elmundoeats.com/wp-content/uploads/2020/12/FP-10-Minute-Eggless-Tiramisu-For-Two-2.jpg",
+        price: "$6.00"
+    },
+]
